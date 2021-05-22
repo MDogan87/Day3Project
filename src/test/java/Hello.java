31e26eb3850a8bbd599ba56f362c556aacbf740d
MDogan <mdoganusa@gmail.com>
2021-05-22 11:26:24 -0400

    added new

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Helloooo Worlllddd");
+
+        // we runned hello
+        System.out.println("new thing");
     }
 }
