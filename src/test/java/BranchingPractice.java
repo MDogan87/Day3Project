a9ce7fc718393570b2386afb02fa549ec04fd8ea
MDogan <mdoganusa@gmail.com>
2021-05-25 19:26:12 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,9 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+         //created new java class
+        //added my folder on git
+        //made initial commit
+        //
 
     }
 }
